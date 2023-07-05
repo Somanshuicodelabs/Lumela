c360461fc9acc977c0f5d061bfc0f9f75cf1d4d5
SomanshuIcodelabs <somanshu.sharma@icodelabs.co>
2023-07-05 12:46:42 +0530

    changes

--- src/containers/SearchPage/SearchResultsPanel/SearchResultsPanel.js
+++ src/containers/SearchPage/SearchResultsPanel/SearchResultsPanel.js
@@ -58,7 +58,7 @@ const SearchResultsPanel = props => {
     <div className={classes}>
       <div className={isMapVariant ? css.listingCardsMapVariant : css.listingCards}>
       {/* // const listing = listings.filter(item => item?.attributes?.publicData?.abn === "abn") */}
-        {listings.map(l => (
+        {listings.filter(item => item?.attributes?.publicData?.listingType === "business").map(l => (
           <ListingCard
             className={css.listingCard}
             key={l.id.uuid}
